206d32912ab7d11e6ebf2f74668e827949360c0e
iclxy <clarislim.cl@gmail.com>
2025-06-01 16:03:24 +0800

    updated pipeline.py

--- spaceflights/src/spaceflights/pipelines/pipeline.py
+++ spaceflights/src/spaceflights/pipelines/pipeline.py
@@ -4,12 +4,12 @@ from .nodes import preprocess_data, split_data, train_model, evaluate_model
 
 def create_pipeline(**kwargs) -> Pipeline:
     """
-    Five-node pipeline:
-      01_preprocess_data  = compute 'repeat_buyer'
-      02_split_data       = filter numeric features & split
-      03_train_model      = fit the XGBClassifier
-      04_evaluate_model   = compute metrics on the test split
-      05_save_model       = persist the model into the catalog
+    Five-node pipeline in order:
+      preprocess_data  = compute 'repeat_buyer'
+      split_data       = filter numeric features & split
+      train_model      = fit the XGBClassifier
+      evaluate_model   = compute metrics on the test split
+      save_model       = persist the model into the catalog
     """
     return Pipeline(
         [
@@ -17,32 +17,32 @@ def create_pipeline(**kwargs) -> Pipeline:
                 func=preprocess_data,
                 inputs=["final_merged_olist_with_geolocation", "parameters"],
                 outputs="preprocessed_data",
-                name="01_preprocess_data",
+                name="preprocess_data",
             ),
             node(
                 func=split_data,
                 inputs=["preprocessed_data", "parameters"],
                 outputs=["X_train", "X_test", "y_train", "y_test"],
-                name="02_split_data",
+                name="split_data",
             ),
             node(
                 func=train_model,
                 inputs=["X_train", "y_train", "parameters"],
                 outputs="model",
-                name="03_train_model",
+                name="train_model",
             ),
             node(
                 func=evaluate_model,
                 inputs=["model", "X_test", "y_test"],
                 outputs="metrics",
-                name="04_evaluate_model",
+                name="evaluate_model",
             ),
             node(
-                # lambda simply returns the model object so Kedro can write it via the catalog:
+                # lambda returns model object so Kedro can write it via the catalog:
                 func=lambda model: model,
                 inputs="model",
                 outputs="saved_model_xgboost",
-                name="05_save_model",
+                name="save_model",
             ),
         ]
     )
