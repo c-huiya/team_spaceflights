2356f3794d8747b5712539bf8110c30046d49423
iclxy <clarislim.cl@gmail.com>
2025-06-04 22:56:40 +0800

    updated run.sh

--- spaceflights/run.sh
+++ spaceflights/run.sh
@@ -3,8 +3,8 @@ set -e
 # "set -e" tells Bash to exit if any command fails.
 
 # Build/rebuild Docker image whenever the code/requirements change:
-docker build -t spaceflights:image .
+docker build -t spaceflights-image .
 
 # Run the container. Inside the container, `kedro run` is the default CMD.
 # The part "--rm" ensures Docker delete the container as soon as it exits so there won't be alot of unused containers.
-docker run --rm spaceflights:image
+docker run --rm spaceflights-image
